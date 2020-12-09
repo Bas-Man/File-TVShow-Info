116c937cf1665c0e7da23ec72fda56085535b1e1
Adam Spann <Bas-Man@users.noreply.github.com>
2020-12-09 22:51:44 +0900

    New Release given rt.cpan.org is being sunset.

--- lib/File/TVShow/Info.pm
+++ lib/File/TVShow/Info.pm
@@ -14,11 +14,11 @@ File::TVShow::Info - Perl meta data extractor from file name for TV Show file.
 
 =head1 VERSION
 
-Version 0.01.1.0
+Version 0.01.2
 
 =cut
 
-our $VERSION = '0.01.1.0';
+our $VERSION = '0.01.2';
 
 
 =head1 SYNOPSIS
